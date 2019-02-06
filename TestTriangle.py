e0efd0c64319462b656d5687967e9aa78eebfae7
nnekaou <42782069+nnekaou@users.noreply.github.com>
2019-02-05 23:32:03 -0500

    Add files via upload

---
+++ TestTriangle.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Updated Jan 21, 2018
+The primary goal of this file is to demonstrate a simple unittest implementation
+
+@author: jrr
+@author: rk
+"""
+
+import unittest
+
+from Triangle import classifyTriangle
+
+# This code implements the unit test functionality
+# https://docs.python.org/3/library/unittest.html has a nice description of the framework
+
+class TestTriangles(unittest.TestCase):
+    # define multiple sets of tests as functions with names that begin
+    #right triangles
+    def testRightTriangleA(self): 
+        self.assertEqual(classifyTriangle(3,4,5),'Right','3,4,5 is a Right triangle')
+
+    def testRightTriangleB(self): 
+        self.assertEqual(classifyTriangle(5,3,4),'Right','5,3,4 is a Right triangle')
+    
+    #equilateral triangles
+    def testEquilateralTriangleA(self): 
+        self.assertEqual(classifyTriangle(1,1,1),'Equilateral','1,1,1 should be equilateral')
+    
+    def testEquilateralTriangleB(self):
+        self.assertEqual(classifyTriangle(7,7,7),'Equilateral','7,7,7 should be equilateral')
+    
+    #isosceles triangles
+    def testIsoscelesTriangleA(self):
+        self.assertEqual(classifyTriangle(3,4,4), 'Isosceles', '3,4,4 should be isosceles')
+    
+    def testIsoscelesTriangleB(self):
+        self.assertEqual(classifyTriangle(6,5,6), 'Isosceles', '6,5,6 should be isosceles')
+
+    #scalene triangles
+    def testScaleneTriangleA(self):
+        self.assertEqual(classifyTriangle(5,6,7), 'Scalene', '5,6,7 is scalene')
+    def testScaleneTriangleB(self):
+        self.assertEqual(classifyTriangle(8,7,6), 'Scalene', '9,1,3 is scalene')
+
+if __name__ == '__main__':
+    print('Running unit tests')
+    unittest.main()
